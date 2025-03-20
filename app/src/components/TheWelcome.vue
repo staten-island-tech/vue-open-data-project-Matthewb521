26fd6c4baab0f5b873e13f7e30196684d60d40d7
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2025-03-20 12:27:15 -0400

    a

---
+++ app/src/components/TheWelcome.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex justify-center items-center">
+    <img
+      src="https://th.bing.com/th/id/OIP.CScKTOrUlEaqkd9PM53riQHaE8?w=242&h=180&c=7&r=0&o=5&pid=1.7"
+      alt=""
+      class="w-475 h-202"
+    />
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="flex justify-center items-center">
-    <img
-      src="https://th.bing.com/th/id/OIP.CScKTOrUlEaqkd9PM53riQHaE8?w=242&h=180&c=7&r=0&o=5&pid=1.7"
-      alt=""
-      class="w-475 h-202"
-    />
+  <div>
+    <TheWelcome />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
 
 <style scoped></style>
